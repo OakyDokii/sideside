c7a96c237b26b56c9d3d4e5d506017f904301b5e
Bridget B <oakydokii@gmail.com>
2024-02-21 09:45:45 -0500

    Adjust jumping calculation to only allow second jump when in freefall

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -20,13 +20,16 @@ func _process(_delta) :
 
 func _physics_process(delta) :
 	# Add the gravity.
-	if not is_on_floor():
+	if not is_on_floor() :
 		velocity.y += (gravity * gravityAdjustment) * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and jumpCounter > 0 :
 		velocity.y = JUMP_VELOCITY
-		jumpCounter -= 1
+		if not is_on_floor() :
+			jumpCounter -= 2
+		else :
+			jumpCounter -= 1
 
 	if Input.is_action_pressed("sprint") :
 		sprintAdjustment = 1.5
@@ -36,9 +39,9 @@ func _physics_process(delta) :
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
+	if direction :
 		velocity.x = direction * (SPEED * sprintAdjustment)
-	else:
+	else :
 		velocity.x = move_toward(velocity.x, 0, (SPEED * sprintAdjustment))
 
 	move_and_slide()
