d8e485faceb9f4c370e33bc1d6fee40d840fcdf9
Bridget B <oakydokii@gmail.com>
2024-02-20 22:36:56 -0500

    Add basic sprint functionality

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -3,17 +3,20 @@ extends CharacterBody2D
 const SPEED = 200.0
 const JUMP_VELOCITY = -200.0
 
-var jumpCounter = 2;
+var jumpCounter = 2
+var sprintAdjustment = 1.00
+
+var finalSpeed = SPEED * sprintAdjustment
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 # Adjustment value for gravity
-var gravityAdjustment = 0.5;
+var gravityAdjustment = 0.5
 
 func _process(_delta) :
 	if is_on_floor() :
-		jumpCounter = 2;
+		jumpCounter = 2
 
 func _physics_process(delta) :
 	# Add the gravity.
@@ -25,12 +28,17 @@ func _physics_process(delta) :
 		velocity.y = JUMP_VELOCITY
 		jumpCounter -= 1
 
+	if Input.is_action_pressed("sprint") :
+		sprintAdjustment = 1.5
+	else :
+		sprintAdjustment = 1.0
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * (SPEED * sprintAdjustment)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, (SPEED * sprintAdjustment))
 
 	move_and_slide()
