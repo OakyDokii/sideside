f46f794bc42167cf09cfdef6098cadf690dca474
Bridget B <oakydokii@gmail.com>
2024-03-06 10:29:29 -0500

    Replace default ui actions with custom actions per the good practice guidelines

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 200.0
+const SPEED = 100.0
 const JUMP_VELOCITY = -200.0
 
 var jumpCounter = 2
@@ -24,7 +24,7 @@ func _physics_process(delta):
 		velocity.y += (gravity * gravityAdjustment) * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and jumpCounter > 0:
+	if Input.is_action_just_pressed("jump") and jumpCounter > 0:
 		velocity.y = JUMP_VELOCITY
 		if not is_on_floor():
 			jumpCounter -= 2
@@ -38,7 +38,7 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move left", "move right")
 	if direction:
 		velocity.x = direction * (SPEED * sprintAdjustment)
 	else:
